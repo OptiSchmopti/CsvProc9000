c1d3edaf308da4f50d43dc6c0839a3d876b2cd1c
Martin Wagenführ <martin@wgnf.de>
2021-11-02 13:42:34 +0100

    just some stupid stuff

---
+++ src/CsvProc9000/Hello.cs
@@ -0,0 +1,13 @@
+﻿namespace CsvProc9000
+{
+    public class Hello
+    {
+        public int Do(int x)
+        {
+            var result = 1 + 1;
+            var anotherResult = 2 + result;
+
+            return anotherResult + x;
+        }
+    }
+}
