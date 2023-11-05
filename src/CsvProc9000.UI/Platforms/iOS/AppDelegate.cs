bb67cef71d9428ebb385a61fe9ade6a85c016c11
Martin Wagenführ <martin@wgnf.de>
2023-11-05 14:31:48 +0100

    chore: remove unsupported platforms

--- src/CsvProc9000.UI/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CsvProc9000.UI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
